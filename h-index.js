8858b8931558aceb50a5e6c939ee657ab48ed219
kdtTetz <xenosign@naver.com>
2024-11-20 16:05:39 +0900

    241120 마지막 리턴 수정

--- h-index.js
+++ h-index.js
@@ -4,17 +4,19 @@ function solution(citations) {
   let answer = 0;
 
   const sortedArr = citations.sort((a, b) => a - b);
-  let arrLen = sortedArr.length;
+  let sortedArrLen = sortedArr.length;
 
   for (let i = 0; i < sortedArr.length; i++) {
-    answer = arrLen - i;
+    answer = sortedArrLen - i;
     if (sortedArr[i] >= answer) return answer;
   }
 
-  return answer;
+  return 0;
 }
 
 const ex1 = [3, 0, 6, 1, 5];
 const ex2 = [0, 5, 6, 7, 8];
+const ex3 = [0, 0, 0];
 console.log(solution(ex1)); // 3
 console.log(solution(ex2)); // 4
+console.log(solution(ex3)); // 0
